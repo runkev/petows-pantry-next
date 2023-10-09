77f797a083eddf2e87a0c5a978ba641a1cc5b497
kpetow <92036249+kpetow@users.noreply.github.com>
2023-10-09 16:29:41 -0500

    commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,25 +7,25 @@ export default async function Home() {
 
   return (
     <div>
-      <h1 className="text-7xl font-extrabold">Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Kapehe!</span>
+      <h1 className="">Hello I&apos;m
+        <span className=""> Kapehe!</span>
       </h1>
-      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my recipes!</p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My recipes</h2>
+      <p className="">Aloha everyone! Check out my recipes!</p>
+      <h2 className="">My recipes</h2>
 
-      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="">
         {recipes.map((recipe) => (
-        <Link href={`/recipes/${recipe.slug}`} key={recipe._id} className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
+        <Link href={`/recipes/${recipe.slug}`} key={recipe._id} className="">
           {recipe.image && (
             <Image
               src={recipe.image}
               alt={recipe.name}
               width={750}
               height={300}
-              className="object-cover rounded-lg border border-gray-500"
+              className=""
             />
           )}
-          <div className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          <div className="">
             {recipe.name}
           </div>
         </Link>
--- app/(site)/recipes/[recipe]/page.tsx
+++ app/(site)/recipes/[recipe]/page.tsx
@@ -11,14 +11,14 @@ export default async function Recipe({ params }: Props) {
   const recipe = await getRecipe(slug);
 
   return <div>
-    <header className="flex items-center justify-between">
-      <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{recipe.name}</h1>
+    <header className="">
+      <h1 className="">{recipe.name}</h1>
     </header>
 
-    <div className="text-lg text-gray-700 mt-5">
+    <div className="">
       <PortableText value={recipe.content} />
     </div>
 
-    <Image src={recipe.image} alt={recipe.name} width={1920} height={1080} className="mt-10 border-2 border-gray-700 object-cover rounded-xl" />
+    <Image src={recipe.image} alt={recipe.name} width={1920} height={1080} className="" />
   </div>
 } 
