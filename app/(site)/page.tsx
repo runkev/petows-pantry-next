84a7f7b9ea16a1f0322c082fac065e1e5c5b72fb
kpetow <kevin.m.petow@outlook.com>
2023-10-09 10:47:55 -0400

    tailwind config, navbar format, recipe schema

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <h1 className="text-7xl font-extrabold">Hello I&apos;m
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Kapehe!</span>
       </h1>
-      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my recipes!</p>
+      <p className="mt-3 text-xl text-gray-600">Check out my recipes!</p>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My recipes</h2>
 
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
--- sanity/schemas/page-schema.ts
+++ sanity/schemas/page-schema.ts
@@ -9,6 +9,11 @@ const page = {
         type: "string"
       },
       {
+        name: "subtitle",
+        title: "Subtitle",
+        type: "string"
+      },
+      {
         name: "slug",
         title: "Slug",
         type: "slug",
