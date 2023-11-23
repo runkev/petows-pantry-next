e864919265c4f4138eb7841fce142a9ddc7aae2b
kpetow <kevin.m.petow@outlook.com>
2023-11-23 12:12:07 -0500

    ig link on about page

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/banner";
 import { getPage } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+import { FaInstagram } from "react-icons/fa";
 
 type Props = {
   params: { slug: string }
@@ -15,6 +16,16 @@ export default async function Page({ params }: Props) {
           title={page.title}
       />
       <div className="prose container mx-auto px-4 py-4 max-w-5xl"><PortableText value={page.content} /></div>
+      <div>
+        <a
+            href="https://www.instagram.com/kevinpetow"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-800 hover:text-cookie flex justify-center my-8"
+          >
+            <FaInstagram className="inline-block w-6 h-6 mr-1" />
+          </a>
+      </div>
     </div>
   )
 }
